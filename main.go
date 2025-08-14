628ba99ea145c54a66d601e511b263e8765e3907
Pranay0205 <pranayghuge@gmail.com>
2025-08-14 00:02:17 -0400

    feat: add middleware to collect metrics of user visitations

--- main.go
+++ main.go
@@ -4,9 +4,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request){
+		log.Printf("📃	%s %s - User-Agent: %s", r.Method, r.URL.String(),	r.UserAgent())
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	w.WriteHeader(http.StatusOK)
+
+	count := cfg.fileserverHits.Load()
+
+	message := fmt.Sprintf("Hits: %d", count)
+
+	w.Write([]byte(message))
+}
 
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,19 +41,26 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
+
+
 func main() {
 
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	apiConfig := apiConfig{fileserverHits: atomic.Int32{},}
 
 	fmt.Println("🚀  Server Starting...")
 	fmt.Println("🕒  Time:", time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Println("💡  Tip: Press Ctrl+C to stop")
 	
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", http.StripPrefix("/app/", apiConfig.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	
 	mux.HandleFunc("/healthz", healthHandler)
+
+	mux.HandleFunc("/metrics", apiConfig.handleMetrics)
+	mux.HandleFunc("/reset", apiConfig.handleResetMetrics)
 
 	server := &http.Server{Addr: ":" + port, Handler: mux}
 
